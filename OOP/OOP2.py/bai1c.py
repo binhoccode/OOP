c668a2caacfb8ef4b433507f1bb46e0f85a5336b
binhoccode <25112052@st.vju.ac.vn>
2026-05-14 19:49:29 +0700

    Delete OOP directory

--- OOP/OOP2.py/bai1c.py
+++
@@ -1,8 +0,0 @@
-time_per_mile_sec = int(input("Enter time per mile in seconds: "))
-pace_minutes = int(time_per_mile_sec // 60)
-pace_seconds = int(time_per_mile_sec % 60)
-print(f"Average pace per mile: {pace_minutes} minutes {pace_seconds} seconds")
-total_hours = time_per_mile_sec / 3600
-mile = 1
-speed_mph = mile / total_hours
-print(f"Average speed is: {speed_mph:.2f} mph")
